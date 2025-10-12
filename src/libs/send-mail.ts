60a497706f51b13d1beb958130f08b2be48f94c0
Cường Dương Vũ Thái <cuong.dvt@anlab.asia>
2025-10-12 18:25:28 +0700

    Add sendmail lib

---
+++ src/libs/send-mail.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import nodemailer from 'nodemailer';
+import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+// Looking to send emails in production? Check out our Email API/SMTP product!
+const transporter = nodemailer.createTransport({
+  // Looking to send emails in production? Check out our Email API/SMTP product!
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '586005919f591a',
+    pass: 'd29a0c11dcecc3',
+  },
+});
+
+interface ISendmailBody {
+  email: string;
+  sendTo?: string;
+  subject: string;
+  text: string;
+}
+
+const sendMail = async (props: ISendmailBody): Promise<SMTPTransport.SentMessageInfo | null> => {
+  try {
+    await transporter.verify();
+  } catch (error) {
+    console.error('Send mail - Something Went Wrong', error);
+    return null;
+  }
+  return await transporter.sendMail({
+    from: props.email,
+    to: props.sendTo,
+    subject: props.subject,
+    text: props.text,
+  });
+};
+
+export default sendMail;
